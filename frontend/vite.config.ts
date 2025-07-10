42b3df4caaaab1c7ec0f1d9f116e2e88acd1a627
jeetpatel11 <pateljeetr5012@gmail.com>
2025-07-10 18:21:55 +0530

    dump

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,19 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
 export default defineConfig({
-  plugins: [react(), ],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': resolve(__dirname, './src'),
     },
   },
   server: {
-		port: 3000,
-	},
+    port: 3000,
+  },
 })
